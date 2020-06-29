8970ab3f8476827092f9e077d614168982053401
AShoom85 <a.shoomskiy@gmail.com>
2020-06-29 12:40:49 +0300

    commit

--- DemoRest/DemoRest/settings.py
+++ DemoRest/DemoRest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iz4$%+qg4wwz2yt&&y8p6=%#zw1iyy70w0)9#azv15#2vd56%4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
